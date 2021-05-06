e189b6698ec34ed3a230a898ecd680a4487bbe14
Felix Green <work@felix.green>
2021-05-06 19:23:16 -0400

    add head stuff

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,10 +1,10 @@
 <script>
 	import '../global.css'
-
 </script>
 
+<svelte:head>
+	<title>hexis.wtf</title>
+	<script src="https://cdn.usefathom.com/script.js" data-site="ARWXVRAM" data-excluded-domains="localhost" defer></script>
+</svelte:head>
+
 <slot />
-
-<style global style>
-
-</style>
