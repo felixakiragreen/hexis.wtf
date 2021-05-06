65e9dd834d915e7496ba584933c1b5225a5cd504
Felix Green <work@felix.green>
2021-05-06 19:39:48 -0400

    put the header in the layout

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,10 +1,14 @@
 <script>
 	import '../global.css'
+
+	import Header from './_header.svelte'
 </script>
 
 <svelte:head>
 	<title>hexis.wtf</title>
 	<script src="https://cdn.usefathom.com/script.js" data-site="ARWXVRAM" data-excluded-domains="localhost" defer></script>
 </svelte:head>
+
+<Header />
 
 <slot />
