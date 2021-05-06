d4f9ee0379c19ed6b67b8faca2eb39a25c9ef869
Felix Green <work@felix.green>
2021-05-06 19:39:25 -0400

    style the header

---
+++ src/routes/_header.svelte
@@ -0,0 +1,34 @@
+<script>
+
+	import { css } from '@/ui'
+  import Content from '$lib/Content.svelte'
+	import Socials from '$lib/Social/index.svelte'
+	
+	const ss = css({
+		backgroundColor: "$background",
+    
+    "& p": {
+      color: "$muted",
+    },
+    "& div": {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+    }
+	})
+
+</script>
+
+
+<header class={ss}>
+  <Content>
+    <div>
+      <p>
+        new episode every <b>friday — 09:00 et</b>
+      </p>
+
+      <Socials />
+    </div>
+  </Content>
+</header>
