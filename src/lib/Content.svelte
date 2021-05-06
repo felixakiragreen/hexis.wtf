da1de2d9cbb8e387f1cddc1115bb5dde1fe14d6e
Felix Green <work@felix.green>
2021-05-06 19:39:36 -0400

    add a content section

---
+++ src/lib/Content.svelte
@@ -0,0 +1,31 @@
+<script>
+
+	import { css } from '@/ui'
+
+	
+	const ss = css({
+		// backgroundColor: "$background",
+    // color: "$foreground",
+
+    mx: 'auto',
+    maxWidth: '$screen-lg',
+    py: '$4',
+
+    '@initial': {
+      px: '$4',
+    },
+    '@sm': {
+      px: '$8',
+    },
+    '@xl': {
+      px: '$0',
+    },
+    
+	})
+
+</script>
+
+
+<section class={ss}>
+  <slot />
+</section>
