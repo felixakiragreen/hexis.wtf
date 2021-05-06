345c6f9e75cb9c012853236e8986e07c9716b966
Felix Green <work@felix.green>
2021-05-06 19:23:23 -0400

    button test

---
+++ src/lib/Button.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { css } from '@/ui'
+	
+	const buttonStyles = css({
+		backgroundColor: '$colors$felixgreen',
+		borderRadius: '9999px',
+		fontSize: '$3',
+		lineHeight: '1',
+		fontWeight: 500,
+		paddingTop: '10px',
+		paddingBottom: '10px',
+		paddingLeft: '16px',
+		paddingRight: '16px',
+		border: '0',
+    cursor: "pointer",
+
+		// Pseudo-class selector
+		'&:hover': {
+			transform: 'translateY(-2px)',
+			boxShadow: '0 10px 25px rgba(0, 0, 0, .3)',
+		},
+	})
+
+</script>
+
+<button class={buttonStyles}>Button</button>
