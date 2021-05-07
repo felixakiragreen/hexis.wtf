483b6389f3199689bb63c16c803ef35ce371ed47
Felix Green <work@felix.green>
2021-05-07 15:02:00 -0400

    header experiments

--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -1,34 +1,38 @@
-<script>
-
-	import { css } from '@/ui'
-  import Content from '$lib/Content.svelte'
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import Content from '$lib/Content.svelte'
 	import Socials from '$lib/Social/index.svelte'
-	
-	const ss = css({
-		backgroundColor: "$background",
-    
-    "& p": {
-      color: "$muted",
-    },
-    "& div": {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-    }
+	import Box from '$lib/Box.svelte'
+
+	const ss = stitch({
+		backgroundColor: '$background',
+
+		'& p': {
+			color: '$muted',
+			textAlign: 'center',
+		},
+		'& div': {
+			// display: 'flex',
+			// flexDirection: 'row',
+			// alignItems: 'center',
+			// justifyContent: 'space-between',
+		},
 	})
 
+	console.log(ss())
 </script>
 
+<header class={ss()}>
+	<Content css={{ position: 'relative' }}>
+		<div>
+			<p>
+				new episode every
+				<b>friday — 09:00 et</b>
+			</p>
 
-<header class={ss}>
-  <Content>
-    <div>
-      <p>
-        new episode every <b>friday — 09:00 et</b>
-      </p>
-
-      <Socials />
-    </div>
-  </Content>
+		</div>
+		<Box css={{ position: 'absolute', top: '$4', right: '$0' }}>
+			<Socials />
+		</Box>
+	</Content>
 </header>
