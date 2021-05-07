02cfff52cb2a01c96a2381dbb52899b941dbe37b
Felix Green <work@felix.green>
2021-05-07 15:01:20 -0400

    add box component

---
+++ src/lib/Box.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { stitch } from '@/ui'
+
+	// export let color
+	export let cls = null
+	export let css = null
+	export let vrt = {}
+
+	const ss = cls || stitch({})
+</script>
+
+<div class={ss({ ...vrt, css })}>
+	<slot />
+</div>
