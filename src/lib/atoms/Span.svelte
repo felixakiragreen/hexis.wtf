16d29d0da41ef1bfe9497cf447d156e6b7ac8fb6
Felix Green <work@felix.green>
2021-05-08 20:20:20 -0400

    add text components

---
+++ src/lib/atoms/Span.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+
+	export let cls = null
+	export let css = null
+	export let style = null
+	export let vrt = {}
+
+	const ss = cls || stitch({})
+</script>
+
+<span class={ss({ ...vrt, css })} {style}>
+	<slot />
+</span>
