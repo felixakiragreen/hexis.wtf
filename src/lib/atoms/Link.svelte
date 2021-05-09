6b9030d36f5c1572bfd574c71e9891e9838205a3
Felix Green <work@felix.green>
2021-05-08 20:20:47 -0400

    add stuffs for link

--- src/lib/atoms/Link.svelte
+++ src/lib/atoms/Link.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import { stitch } from '@/ui'
+
 	export let url
 	export let newTab = false
+	let target = newTab ? '_blank' : '_self'
+
+	export let cls = null
+	export let css = null
+	export let style = null
+	export let vrt = {}
+	const ss = cls || stitch({})
 </script>
 
-<a href={url} target={newTab ? '_blank' : '_self'}>
+<a href={url} {target} class={ss({ ...vrt, css })} {style}>
 	<slot />
 </a>
