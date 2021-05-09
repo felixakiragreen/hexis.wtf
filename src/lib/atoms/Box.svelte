2974509333b9d6655f357fb52a31179cc796a710
Felix Green <work@felix.green>
2021-05-08 20:20:36 -0400

    add style prop

--- src/lib/atoms/Box.svelte
+++ src/lib/atoms/Box.svelte
@@ -4,11 +4,12 @@
 	// export let color
 	export let cls = null
 	export let css = null
+	export let style = null
 	export let vrt = {}
 
 	const ss = cls || stitch({})
 </script>
 
-<div class={ss({ ...vrt, css })}>
+<div class={ss({ ...vrt, css })} {style}>
 	<slot />
 </div>
