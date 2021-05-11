6201954c72669e17f0f5f6d712e8354f8e276f63
Felix Green <work@felix.green>
2021-05-11 15:08:06 -0400

    cleanup footer

--- src/routes/_footer.svelte
+++ src/routes/_footer.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
-	import { stitch } from '@/ui'
 	import Content from '@/lib/bonds/Content.svelte'
-	import Box from '@/lib/atoms/Box.svelte'
-	import Stack from '@/lib/bonds/Stack.svelte'
 	import Socials from '@/lib/comps/Socials.svelte'
-
-	const ss = stitch({
-		backgroundColor: '$background',
-		color: '$foreground',
-		textAlign: 'center',
-		text: '$lg',
-	})
 </script>
 
-<!-- prettier-ignore -->
-
-<footer class={ss()}>
-	<Content>
+<footer>
+	<Content css={{ color: '$foreground', py: '$12' }}>
 		<Socials align="center" size="lg" />
 	</Content>
 </footer>
