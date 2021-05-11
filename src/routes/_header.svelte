249cde72852225e84e018e15703160d561812154
Felix Green <work@felix.green>
2021-05-11 16:28:56 -0400

    cleanup

--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -1,41 +1,15 @@
 <script lang="ts">
-	import { stitch } from '@/ui'
 	import Content from '@/lib/bonds/Content.svelte'
 	import Socials from '@/lib/comps/Socials.svelte'
-	import Box from '@/lib/atoms/Box.svelte'
-	import Stack from '@/lib/bonds/Stack.svelte'
 	import Inline from '@/lib/bonds/Inline.svelte'
 	import Text from '@/lib/bonds/Text.svelte'
 	import Link from '@/lib/bonds/Link.svelte'
-
-	const ss = stitch({
-		// backgroundColor: '$background',
-		color: '$foreground',
-		position: 'relative',
-		zIndex: 100,
-
-		'& p': {
-			color: '$muted',
-			textAlign: 'center',
-		},
-		'& div': {
-			// display: 'flex',
-			// flexDirection: 'row',
-			// alignItems: 'center',
-			// justifyContent: 'space-between',
-		},
-	})
-
-	// console.log(ss())
 </script>
 
-<header class={ss()}>
-	<Content css={{ position: 'relative' }}>
-
-		<!-- <Box css={{ position: 'absolute', top: '$4', right: '$0' }}> -->
-		<!-- </Box> -->
-		<Inline align="trailing" alignV="center" collapseBelow="lg" space="xl">
-			<Text>
+<header>
+	<Content>
+		<Inline align="trailing" alignV="center" collapseBelow="lg" space="xl" css={{ py: '$4' }}>
+			<Text css={{ color: '$muted' }}>
 				new episodes
 				<b>friday @ 09:00 et</b>
 			</Text>
