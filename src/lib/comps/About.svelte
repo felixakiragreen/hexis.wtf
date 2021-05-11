a586ec2f9370cc7306b2babe6c534f42107a814b
Felix Green <work@felix.green>
2021-05-11 15:10:05 -0400

    cleanup

--- src/lib/comps/About.svelte
+++ src/lib/comps/About.svelte
@@ -3,7 +3,6 @@
 	import Content from '@/lib/bonds/Content.svelte'
 	import Box from '@/lib/atoms/Box.svelte'
 	import Stack from '@/lib/bonds/Stack.svelte'
-	import Socials from '@/lib/comps/Socials.svelte'
 	import Text from '@/lib/bonds/Text.svelte'
 	import Link from '@/lib/bonds/Link.svelte'
 
