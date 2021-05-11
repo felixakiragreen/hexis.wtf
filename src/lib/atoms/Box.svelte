6c864d32df9bc94825fdba1410e148fe59f0d4e6
Felix Green <work@felix.green>
2021-05-11 12:52:14 -0400

    pass in id for linking

--- src/lib/atoms/Box.svelte
+++ src/lib/atoms/Box.svelte
@@ -5,11 +5,12 @@
 	export let cls = null
 	export let css = null
 	export let style = null
+	export let id = null
 	export let vrt = {}
 
 	const ss = cls || stitch({})
 </script>
 
-<div class={ss({ ...vrt, css })} {style}>
+<div class={ss({ ...vrt, css })} {style} {id}>
 	<slot />
 </div>
