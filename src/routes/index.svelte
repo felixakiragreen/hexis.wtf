2e14e01f6240ce9b38f6280674a3bc05590fdf20
Felix Green <work@felix.green>
2021-05-11 16:08:02 -0400

    cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,11 +7,7 @@
 	import FloatingCubes from '@/lib/comps/FloatingCubes.svelte'
 
 	const ss = stitch({
-		// backgroundColor: '$background',
 		color: '$foreground',
-		// position: 'relative',
-		// overflowX: 'hidden',
-		// maxWidth: '$screen-w',
 	})
 </script>
 
@@ -25,36 +21,4 @@
 
 	<About />
 
-	<!-- <h1 class="desktop">season 01 begins</h1>
-	<h1 class="mobile">s01 begins</h1>
-	<p>
-		<span class="nb">
-			created by
-			<a href="https://twitter.com/felixakiragreen" target="_blank">Felix Green</a>
-		</span>
-		<span class="nb">
-			and
-			<a href="https://twitter.com/Macbeth_wtf" target="_blank">Macbeth</a>
-		</span>
-	</p>
-	<a href="https://app.rarible.com/hexis_wtf?tab=collectibles" target="_blank">
-		<img src={hexisLogo} width="100%" height="100%" alt="Hexis Logo" />
-	</a>
-
-	<p>
-		<span style="color: var(--grey-400);">follow us</span>
-		<span style="color: var(--grey-100);">@hexis_wtf</span>
-	</p>
-
-	<Stack color="light" css={{ p: '$8', backgroundColor: '$blue300' }}>
-		<Button />
-	</Stack> -->
-
-	<!-- <Metadata /> -->
-
-	<!-- {#if mounted} -->
-	<!-- <footer>
-		<Socials />
-	</footer> -->
-	<!-- {/if} -->
 </main>
