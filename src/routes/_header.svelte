8a8714e87aff30cf4f20985ee63b74061b649f6e
Felix Green <work@felix.green>
2021-05-11 19:05:43 -0400

    fix zIndex

--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+	import { stitch } from '@/ui'
+
 	import Content from '@/lib/bonds/Content.svelte'
 	import Socials from '@/lib/comps/Socials.svelte'
 	import Inline from '@/lib/bonds/Inline.svelte'
 	import Text from '@/lib/bonds/Text.svelte'
 	import Link from '@/lib/bonds/Link.svelte'
+
+	const ss = stitch({
+		position: 'relative',
+		zIndex: 100,
+	})
 </script>
 
-<header>
+<header class={ss()}>
 	<Content>
 		<Inline align="trailing" alignV="center" collapseBelow="lg" space="xl" css={{ py: '$4' }}>
 			<Text css={{ color: '$muted' }}>
