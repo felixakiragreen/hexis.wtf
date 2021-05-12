d5cf1c67ff9a5cd3002e4a824a376296878804a0
Felix Green <work@felix.green>
2021-05-12 15:05:57 -0400

    add parallaxing

--- src/lib/comps/FloatingCubes.svelte
+++ src/lib/comps/FloatingCubes.svelte
@@ -3,36 +3,45 @@
 	import Img from '@/lib/atoms/Image.svelte'
 	import Box from '@/lib/atoms/Box.svelte'
 	import hexisLogo from '@/assets/hexis-1920-no-bg.svg'
+
+	let y
+
+	// TODO: remove cubes at lower breakpoint
 </script>
 
-<Box css={{ position: 'absolute', surrounding: 0, overflowY: 'visible' }}>
-	<Box css={{ position: 'relative', overflowX: 'hidden' }}>
-		<Content css={{ position: 'relative', h: '120vh', w: '$screen-w' }}>
-			<Img
-				src={hexisLogo}
-				alt="Hexis Logo"
-				css={{ size: '$80', position: 'absolute', top: '$-4', left: '$-12' }}
-			/>
-			<Img
-				src={hexisLogo}
-				alt="Hexis Logo"
-				css={{ size: '$24', position: 'absolute', top: '$32', right: '$40', blur: '2px' }}
-			/>
-			<Img
-				src={hexisLogo}
-				alt="Hexis Logo"
-				css={{ size: '$56', position: 'absolute', bottom: '$72', left: '$-36' }}
-			/>
-			<Img
-				src={hexisLogo}
-				alt="Hexis Logo"
-				css={{ size: '$160', position: 'absolute', bottom: '$24', right: '$-128' }}
-			/>
-			<Img
-				src={hexisLogo}
-				alt="Hexis Logo"
-				css={{ size: '$20', position: 'absolute', bottom: '$24', left: '$12', blur: '2px' }}
-			/>
-		</Content>
-	</Box>
+<svelte:window bind:scrollY={y} />
+
+<Box
+	css={{ position: 'fixed', surrounding: 0, overflowY: 'visible', zIndex: -1 }}
+>
+	<Img
+		src={hexisLogo}
+		alt="Hexis Logo"
+		css={{ size: '$20', position: 'absolute', top: '$96', left: '$40', blur: '2px' }}
+		style="transform: translate3d(0px, {-y * 0.75}px, 0px)"
+	/>
+	<Img
+		src={hexisLogo}
+		alt="Hexis Logo"
+		css={{ size: '$24', position: 'absolute', top: '$32', right: '$64', blur: '2px' }}
+		style="transform: translate3d(0px, {-y * 0.75}px, 0px)"
+	/>
+	<Img
+		src={hexisLogo}
+		alt="Hexis Logo"
+		css={{ size: '$80', position: 'absolute', top: '$-4', left: '$20' }}
+		style="transform: translate3d(0px, {-y * 1.5}px, 0px)"
+	/>
+	<Img
+		src={hexisLogo}
+		alt="Hexis Logo"
+		css={{ size: '$56', position: 'absolute', bottom: '$16', left: '$-12' }}
+		style="transform: translate3d(0px, {-y * 1.25}px, 0px)"
+	/>
+	<Img
+		src={hexisLogo}
+		alt="Hexis Logo"
+		css={{ size: '$160', position: 'absolute', bottom: '$-12', right: '$-80' }}
+		style="transform: translate3d(0px, {-y * 2}px, 0px)"
+	/>
 </Box>
