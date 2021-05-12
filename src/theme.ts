a07b4d40762519fd08d7be79a73b193836c790ef
Felix Green <work@felix.green>
2021-05-12 15:06:04 -0400

    adjust lineheights

--- src/theme.ts
+++ src/theme.ts
@@ -271,10 +271,10 @@ export const tokens = {
 		sm: '1.25rem',
 		md: '1.5rem',
 		lg: '1.75rem',
-		xl: '1.75rem',
-		'2xl': '2rem',
-		'3xl': '2.25rem;',
-		'4xl': '2.5rem',
+		xl: '2rem',
+		'2xl': '2.25rem',
+		'3xl': '2.5rem;',
+		'4xl': '2.75rem',
 		'5xl': '1',
 		'6xl': '1',
 		'7xl': '1',
@@ -302,8 +302,10 @@ export const tokens = {
 		sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
 		base: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
 		md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-		lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-		xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+		lg:
+			'0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+		xl:
+			'0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
 		'2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
 		'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
 		inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
@@ -424,7 +426,8 @@ export const utils = {
 	}),
 	clip: (config) => (value) => {
 		const values = {
-			hexagon: 'polygon(50% 0, 93.3% 25%, 93.3% 75%, 50% 100%, 6.7% 75%, 6.7% 25%)',
+			hexagon:
+				'polygon(50% 0, 93.3% 25%, 93.3% 75%, 50% 100%, 6.7% 75%, 6.7% 25%)',
 		}
 
 		return {
