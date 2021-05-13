de6dae9a36fd2c1ea8b2006863f4161d79fe44b4
Felix Green <work@felix.green>
2021-05-13 14:51:35 -0400

    fix positioning

--- src/lib/comps/FloatingCubes.svelte
+++ src/lib/comps/FloatingCubes.svelte
@@ -7,6 +7,51 @@
 	let y
 
 	// TODO: remove cubes at lower breakpoint
+
+	const cssHex1 = {
+		//
+		size: '$20',
+		position: 'absolute',
+		top: '$96',
+		left: 'calc(50% - 520px)',
+		blur: '2px',
+	}
+
+	const cssHex2 = {
+		//
+		size: '$24',
+		position: 'absolute',
+		top: '$32',
+		right: 'calc(50% - 424px)',
+		blur: '2px',
+		// display: 'none',
+	}
+
+	const cssHex3 = {
+		//
+		size: '$80',
+		position: 'absolute',
+		top: '$-4',
+		left: 'calc(50% - 600px)',
+		// display: 'none',
+	}
+
+	const cssHex4 = {
+		//
+		size: '$56',
+		position: 'absolute',
+		top: '$128',
+		left: 'calc(50% - 724px)',
+		// display: 'none',
+	}
+
+	const cssHex5 = {
+		//
+		size: '$160',
+		position: 'absolute',
+		top: '$48',
+		right: 'calc(50% - 1000px)',
+	}
 </script>
 
 <svelte:window bind:scrollY={y} />
@@ -17,31 +62,31 @@
 	<Img
 		src={hexisLogo}
 		alt="Hexis Logo"
-		css={{ size: '$20', position: 'absolute', top: '$96', left: '$40', blur: '2px' }}
+		css={cssHex1}
 		style="transform: translate3d(0px, {-y * 0.75}px, 0px)"
 	/>
 	<Img
 		src={hexisLogo}
 		alt="Hexis Logo"
-		css={{ size: '$24', position: 'absolute', top: '$32', right: '$64', blur: '2px' }}
+		css={cssHex2}
 		style="transform: translate3d(0px, {-y * 0.75}px, 0px)"
 	/>
 	<Img
 		src={hexisLogo}
 		alt="Hexis Logo"
-		css={{ size: '$80', position: 'absolute', top: '$-4', left: '$20' }}
+		css={cssHex3}
 		style="transform: translate3d(0px, {-y * 1.5}px, 0px)"
 	/>
 	<Img
 		src={hexisLogo}
 		alt="Hexis Logo"
-		css={{ size: '$56', position: 'absolute', bottom: '$16', left: '$-12' }}
+		css={cssHex4}
 		style="transform: translate3d(0px, {-y * 1.25}px, 0px)"
 	/>
 	<Img
 		src={hexisLogo}
 		alt="Hexis Logo"
-		css={{ size: '$160', position: 'absolute', bottom: '$-12', right: '$-80' }}
+		css={cssHex5}
 		style="transform: translate3d(0px, {-y * 2}px, 0px)"
 	/>
 </Box>
