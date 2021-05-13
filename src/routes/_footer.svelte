36cb64ace7a721918dc8391b50b8973248c53dde
Felix Green <work@felix.green>
2021-05-13 14:51:20 -0400

    add breakpoints

--- src/routes/_footer.svelte
+++ src/routes/_footer.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Content from '@/lib/bonds/Content.svelte'
 	import Socials from '@/lib/comps/Socials.svelte'
+
+	const cssLnk = {
+		'@initial': {
+			'> *': { size: '$10' },
+		},
+		'@md': {
+			'> *': { size: '$16' },
+		},
+	}
 </script>
 
 <footer>
 	<Content css={{ color: '$foreground', my: '$16' }}>
-		<Socials align="center" size="lg" />
+		<Socials align="center" css={cssLnk} />
 	</Content>
 </footer>
