5d94ad7935decdc5ddab98f2f495e9f31fce430f
Felix Green <work@felix.green>
2021-05-14 15:02:14 -0400

    remove stupid props

--- src/lib/atoms/Image.svelte
+++ src/lib/atoms/Image.svelte
@@ -1,42 +1,14 @@
 <script>
 	import { stitch } from '@/ui'
 
+	export let cls = null
 	export let css = null
-	export let css_initial = null
-	export let css_sm = null
-	export let css_md = null
 	export let alt = 'image description'
 	export let src
 	export let style = null
 	export let id = null
 
-	const ss = stitch({
-		// css,
-		...(css_initial
-			? {
-					'@initial': {
-						...css_initial,
-						// bg: '$red400',
-					},
-			  }
-			: {}),
-		...(css_sm
-			? {
-					'@sm': {
-						...css_sm,
-						// bg: '$orange400',
-					},
-			  }
-			: {}),
-		...(css_md
-			? {
-					'@md': {
-						...css_md,
-						// bg: '$blue400',
-					},
-			  }
-			: {}),
-	})
+	const ss = cls || stitch({})
 </script>
 
 <img class={ss({ css })} {alt} {src} {style} {id} />
