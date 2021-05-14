27fbcc0511624195272d78dbd8245733b1010e4a
Felix Green <work@felix.green>
2021-05-14 14:53:31 -0400

    experiment with new gap

--- src/lib/bonds/Inline.svelte
+++ src/lib/bonds/Inline.svelte
@@ -4,6 +4,7 @@
 
 	export let css = null
 	export let space = null
+	export let spaceV = null
 	export let align = null
 	export let alignV = null
 	export let collapseBelow = null
@@ -15,7 +16,9 @@
 
 		variants: {
 			space: {
-				no: {},
+				no: {
+					sx: '$0',
+				},
 				sm: {
 					sx: '$2',
 				},
@@ -33,6 +36,29 @@
 				},
 				'3xl': {
 					sx: '$12',
+				},
+			},
+			spaceV: {
+				no: {
+					sy: '$0',
+				},
+				sm: {
+					sy: '$2',
+				},
+				md: {
+					sy: '$4',
+				},
+				lg: {
+					sy: '$6',
+				},
+				xl: {
+					sy: '$8',
+				},
+				'2xl': {
+					sy: '$10',
+				},
+				'3xl': {
+					sy: '$12',
 				},
 			},
 			align: {
@@ -64,25 +90,31 @@
 				sm: {
 					'@initial': {
 						flexDirection: 'column',
+						// sx: '$0',
 					},
 					'@sm': {
 						flexDirection: 'row',
+						// sy: '$0',
 					},
 				},
 				md: {
 					'@initial': {
 						flexDirection: 'column',
+						// sx: '$0',
 					},
 					'@md': {
 						flexDirection: 'row',
+						sy: '$0',
 					},
 				},
 				lg: {
 					'@initial': {
 						flexDirection: 'column',
+						// sx: '$0',
 					},
 					'@lg': {
 						flexDirection: 'row',
+						// sy: '$0',
 					},
 				},
 			},
@@ -94,27 +126,32 @@
 		},
 
 		// TODO: Need to figure out a better way to do this
-		compoundVariants: [
-			{
-				collapseBelow: 'lg',
-				space: 'xl',
-				css: {
-					'@initial': {
-						sy: '$8',
-					},
-					'@lg': {
-						sy: '$0',
-					},
-				},
-			},
-		],
+		// compoundVariants: [
+		// 	{
+		// 		collapseBelow: 'lg',
+		// 		space: 'xl',
+		// 		css: {
+		// 			'@initial': {
+		// 				sy: '$8',
+		// 				bg: '$red400',
+		// 			},
+		// 			'@lg': {
+		// 				sy: '$0',
+		// 				bg: '$orange400',
+		// 			},
+		// 		},
+		// 	},
+		// ],
 	})
 
 	// ss({ css })
 
 	// console.log({ css }, ss({ color, css }))
+
+	// space & spaceV
+	// if collapse below,
 </script>
 
-<Box cls={ss} vrt={{ space, align, alignV, collapseBelow }} {css}>
+<Box cls={ss} vrt={{ space, spaceV, align, alignV, collapseBelow }} {css}>
 	<slot />
 </Box>
