de7f949436f4ea6dbdd4266bb697f1174998d546
Felix Green <work@felix.green>
2021-05-14 22:11:58 -0400

    cleanup button

--- src/lib/atoms/Button.svelte
+++ src/lib/atoms/Button.svelte
@@ -1,25 +1,15 @@
 <script>
 	import { stitch } from '@/ui'
 
-	const buttonStyles = stitch({
-		backgroundColor: '$colors$felixgreen',
-		borderRadius: '9999px',
-		fontSize: '$3',
-		lineHeight: '1',
-		fontWeight: 500,
-		paddingTop: '10px',
-		paddingBottom: '10px',
-		paddingLeft: '16px',
-		paddingRight: '16px',
-		border: '0',
-		cursor: 'pointer',
+	export let cls = null
+	export let css = null
+	export let style = null
+	export let id = null
+	export let vrt = {}
 
-		// Pseudo-class selector
-		'&:hover': {
-			transform: 'translateY(-2px)',
-			boxShadow: '0 10px 25px rgba(0, 0, 0, .3)',
-		},
-	})
+	const ss = cls || stitch({})
 </script>
 
-<button class={buttonStyles}>Button</button>
+<button class={ss({ ...vrt, css })} {style} {id}>
+	<slot />
+</button>
