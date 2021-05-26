e7f7de646d436d0bfb9d330ca979373ebc33bd5f
Felix Green <work@felix.green>
2021-05-25 20:10:33 -0400

    use new ambition component

---
+++ src/lib/comps/Ambition.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { ambition } from '@/lib/datum/socials'
+
+	import Box from '../atoms/Box.svelte'
+	import Anchor from '../atoms/Anchor.svelte'
+	import Image from '../atoms/Image.svelte'
+
+	import ambitionLogo from '@/assets/ambition_sharp_flat_border.svg'
+	import ambitionLogoHover from '@/assets/ambition_sharp_gradient_border.svg'
+
+	const cssBox = {
+		position: 'relative',
+		'#amb-hex-log-hov': {
+			opacity: 0,
+			transition: '$1',
+		},
+		'&:hover': {
+			'#amb-hex-log-hov': {
+				opacity: 1,
+			},
+		},
+	}
+
+	const cssImg = {
+		position: 'absolute',
+		surrounding: 0,
+	}
+</script>
+
+<Anchor url={ambition.url} newTab>
+	<Box css={cssBox}>
+		<Image src={ambitionLogo} alt="Ambition Hexagonal Logo" css={cssImg} />
+		<Image
+			src={ambitionLogoHover}
+			alt="Ambition Hexagonal Logo"
+			css={cssImg}
+			id="amb-hex-log-hov"
+		/>
+	</Box>
+</Anchor>
--- src/lib/comps/Socials.svelte
+++ src/lib/comps/Socials.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import { socials, ambition } from '@/lib/datum/socials'
+	import { socials } from '@/lib/datum/socials'
 
 	import HexIcon from '../bonds/HexIcon.svelte'
 	import Inline from '../bonds/Inline.svelte'
-	import Box from '../atoms/Box.svelte'
-	import Anchor from '../atoms/Anchor.svelte'
-	import Image from '../atoms/Image.svelte'
 
-	import ambitionLogo from '@/assets/ambition_sharp_flat_border.svg'
-	import ambitionLogoHover from '@/assets/ambition_sharp_gradient_border.svg'
+	import Ambition from './Ambition.svelte'
 
 	import { map } from 'lodash'
 	import type { SocialLink } from '@/types'
@@ -19,16 +15,6 @@
 	)
 
 	// console.log({ listSocials })
-
-	// const ss = stitch({
-	// 	display: 'grid',
-	// 	gridTemplateColumns: 'repeat(auto-fill, $16)',
-	// 	w: '$96',
-
-	// 	'& a': {
-	// 		// size: '$16',
-	// 	},
-	// })
 
 	export let space = 'sm'
 	export let align = null
@@ -46,24 +32,6 @@
 	$: iconSize = iconSizes[size] || size
 
 	$: cssInl = css || { '> *': { size: iconSize } }
-
-	const cssBox = {
-		position: 'relative',
-		'#amb-hex-log-hov': {
-			opacity: 0,
-			transition: '$1',
-		},
-		'&:hover': {
-			'#amb-hex-log-hov': {
-				opacity: 1,
-			},
-		},
-	}
-
-	const cssImg = {
-		position: 'absolute',
-		surrounding: 0,
-	}
 </script>
 
 <!-- <Box cls={ss}>
@@ -76,15 +44,5 @@
 	{#each listSocials as social}
 		<HexIcon {...social} />
 	{/each}
-	<Anchor url={ambition.url} newTab>
-		<Box css={cssBox}>
-			<Image src={ambitionLogo} alt="Ambition Hexagonal Logo" css={cssImg} />
-			<Image
-				src={ambitionLogoHover}
-				alt="Ambition Hexagonal Logo"
-				css={cssImg}
-				id="amb-hex-log-hov"
-			/>
-		</Box>
-	</Anchor>
+	<Ambition />
 </Inline>
